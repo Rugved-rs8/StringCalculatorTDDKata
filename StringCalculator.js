a8326aedda0b12f2c79663f294efe0875377b045
Rugved-rs8 <rugvedshahane@gmail.com>
2025-09-06 23:59:42 +0530

    New Line between numbers

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,6 +1,6 @@
 function add(numbers){
     if(numbers === "")return 0;
-    const arr = numbers.split(",");
+    const arr = numbers.split(/[\n,]/);
     if(arr.length == 1) return Number(numbers);
     var ans = 0;
     for(let i=0;i<arr.length;i++){
--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -13,4 +13,7 @@ describe('Testing add method', () => {
     test('multiple numbers', () => {
         expect(add("1,2,3,4,5")).toBe(15);
     });
+    test('new lines between numbers', () => {
+        expect(add("1\n2,3")).toBe(6);
+    });
  });
